fd2db34be67ba372740cb90af36b13e88b045907
nikumar1206 <nikumar1206@gmail.com>
2022-05-24 08:47:20 -0400

    finished phase 3

--- vanilla_dom_manipulation/script.js
+++ vanilla_dom_manipulation/script.js
@@ -28,7 +28,31 @@ document.addEventListener("DOMContentLoaded", () => {
   });
   // can make this on click, turn submit button to just type button
   // --- your code here!
+  document.getElementById("photo_button").addEventListener("click", () => {
+    let photo_form = document.getElementsByClassName("photo-form-container hidden")[0];
+    photo_form.className = "photo-form-container";
+  })
 
+
+  let add_photo_button = document.getElementsByClassName("photo-url-submit")[0]
+
+  document.getElementById("add_photo_form").addEventListener("submit", (event) => {
+    event.preventDefault();
+    let image_url = document.getElementsByClassName("photo-url-input")[0].value;
+
+    let img = document.createElement("img")
+    img.src = image_url
+  
+    let list_photos = document.getElementsByClassName("dog-photos")[0]
+    list_photos.appendChild(img);
+
+
+    //reset url
+    let image_url_reset = document.getElementsByClassName("photo-url-input")[0].value;
+    image_url_reset.value = "added!"
+    
+  });
+  
 
 
   // adding new photos
